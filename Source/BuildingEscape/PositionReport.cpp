94b94e56a7a3ed0c8e1e53fc00ecd41c3e3e33fd
zerowjj <zerowjj@gmail.com>
2020-06-12 15:41:08 +0800

    update get objectname

--- Source/BuildingEscape/PositionReport.cpp
+++ Source/BuildingEscape/PositionReport.cpp
@@ -10,7 +10,7 @@ UPositionReport::UPositionReport()
 	// off to improve performance if you don't need them.
 	PrimaryComponentTick.bCanEverTick = true;
 
-	UE_LOG(LogTemp, Warning, TEXT("Position report reporting for duty on Chair!"));
+	
 }
 
 
@@ -19,7 +19,8 @@ void UPositionReport::BeginPlay()
 {
 	Super::BeginPlay();
 
-	// ...
+	FString ObjectName = GetOwner()->GetName();
+	UE_LOG(LogTemp, Warning, TEXT("Position report reporting for duty on %s!"), *ObjectName);
 	
 }
 
